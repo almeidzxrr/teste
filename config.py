8682bbd6567f74fb76f909d16bdb79f36cf23fcd
almeidzxrr <nada000lu@gmail.com>
2025-10-05 12:21:47 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     # Coloque SEUS dados reais aqui:
-    API_ID = 25983954                          # Seu API ID numérico
-    API_HASH = "34b915d15d26a7319d0b47813a52c855"  # Seu API HASH
-    BOT_TOKEN = "8489578486:AAEUecPBaHun7WBs3B7IBo36SfJ2Qmxt7dk"  # Seu token
+    API_ID = AQUI                          # Seu API ID numérico
+    API_HASH = "Aqui"  # Seu API HASH
+    BOT_TOKEN = "Aqui"  # Seu token
     
     # Lista de administradores (IDs numéricos)
     ADMINS = [6068269833]
